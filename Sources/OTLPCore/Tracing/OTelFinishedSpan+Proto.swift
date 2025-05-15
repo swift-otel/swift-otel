3c643678fc0b6892e4aa2e4f349efe07d02f28cb
Si Beaumont <beaumont@apple.com>
2025-05-15 16:52:12 +0100

    Factor out OTLP conversion logic from GRPC exporter into OTLPCore

--- Sources/OTLPCore/Tracing/OTelFinishedSpan+Proto.swift
+++ Sources/OTLPCore/Tracing/OTelFinishedSpan+Proto.swift
@@ -47,3 +47,22 @@ extension Opentelemetry_Proto_Trace_V1_Span {
         links = finishedSpan.links.compactMap(Opentelemetry_Proto_Trace_V1_Span.Link.init)
     }
 }
+
+extension Opentelemetry_Proto_Trace_V1_ResourceSpans {
+    package init(_ finishedSpans: some Collection<OTelFinishedSpan>) {
+        if let resource = finishedSpans.first?.resource {
+            self.resource = .init(resource)
+        }
+
+        // TODO: This can be factored out somewhere since its used everywhere.
+        let scope = Opentelemetry_Proto_Common_V1_InstrumentationScope.with {
+            $0.name = "swift-otel"
+            $0.version = OTelLibrary.version
+        }
+
+        self.scopeSpans = [Opentelemetry_Proto_Trace_V1_ScopeSpans.with {
+            $0.scope = scope
+            $0.spans = finishedSpans.map(Opentelemetry_Proto_Trace_V1_Span.init)
+        }]
+    }
+}
