b28ab361ca40c199b3d98d35c7fcf0369eadefb0
Si Beaumont <beaumont@apple.com>
2025-07-30 14:55:07 +0100

    Add diagnostic logging to OTelSimpleLogRecordProcessor

--- Sources/OTelCore/Logging/Processing/OTelSimpleLogRecordProcessor.swift
+++ Sources/OTelCore/Logging/Processing/OTelSimpleLogRecordProcessor.swift
@@ -11,35 +11,45 @@
 //
 //===----------------------------------------------------------------------===//
 
+package import Logging
+
 package struct OTelSimpleLogRecordProcessor<Exporter: OTelLogRecordExporter>: OTelLogRecordProcessor {
+    private let logger: Logger
     private let exporter: Exporter
     private let stream: AsyncStream<OTelLogRecord>
     private let continuation: AsyncStream<OTelLogRecord>.Continuation
 
-    package init(exporter: Exporter) {
+    package init(exporter: Exporter, logger: Logger) {
+        self.logger = logger.withMetadata(component: "OTelSimpleLogRecordProcessor")
         self.exporter = exporter
         (stream, continuation) = AsyncStream.makeStream()
     }
 
     package func run() async throws {
+        logger.info("Starting.")
         for try await record in stream.cancelOnGracefulShutdown() {
             do {
+                logger.debug("Exporting log record.")
                 try await exporter.export([record])
             } catch {
                 // simple log processor does not attempt retries
             }
         }
+        logger.info("Log stream ended, shutting down.")
     }
 
     package func onEmit(_ record: inout OTelLogRecord) {
+        logger.trace("Received log record.")
         continuation.yield(record)
     }
 
     package func forceFlush() async throws {
+        logger.info("Force flushing exporter.")
         try await exporter.forceFlush()
     }
 
     package func shutdown() async throws {
+        logger.info("Shutting down.")
         await exporter.shutdown()
     }
 }
