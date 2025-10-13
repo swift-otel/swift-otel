338dfe8d5be7ce5c475f6e606e6014a7a2ce3afa
Moritz Lang <16192401+slashmo@users.noreply.github.com>
2025-10-13 20:38:31 +0200

    Add Hummingbird -> AsyncHTTPClient -> Vapor example

---
+++ Examples/hummingbird-vapor-async-http-client/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "hummingbird-vapor-async-http-client",
+    platforms: [.macOS(.v15)],
+    dependencies: [
+        .package(url: "https://github.com/swift-otel/swift-otel.git", from: "1.0.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+        // TODO: Change to tagged AsyncHTTPClient once a release contains tracing support
+        .package(url: "https://github.com/swift-server/async-http-client.git", branch: "main"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "HummingbirdServer",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "OTel", package: "swift-otel"),
+            ],
+        ),
+        .executableTarget(
+            name: "VaporServer",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "OTel", package: "swift-otel"),
+            ],
+        ),
+    ],
+)
