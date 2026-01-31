b97ab5c343ff673eaca569b27abc5235af48fc27
Moritz Lang <16192401+slashmo@users.noreply.github.com>
2026-01-31 17:59:08 +0100

    Use released async-http-client in AHC example

--- Examples/hummingbird-vapor-async-http-client/Package.swift
+++ Examples/hummingbird-vapor-async-http-client/Package.swift
@@ -8,8 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swift-otel/swift-otel.git", from: "1.0.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        // TODO: Change to tagged AsyncHTTPClient once a release contains tracing support
-        .package(url: "https://github.com/swift-server/async-http-client.git", branch: "main"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.29.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
